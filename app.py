973a8278e77e79b7665060f38e3b31ae9a644619
Farah Boukitab <farahboukitab@gmail.com>
2022-06-03 19:03:59 -0300

    Send last version of streamlit - Friday evening

--- app.py
+++ app.py
@@ -1,33 +1,62 @@
-from sys import base_prefix
+##from sys import base_prefix
+##from turtle import width
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
+import plotly.express as px
 
 ######### Create the user interface #########
 ## Use streamlit for the app
 
 # Define text, title and header
-st.title('Business Case - Le Wagon 869')
-st.header('Prediction and analysis of sales in Favorita stores located in Ecuador')
-st.text('Presentation of our project')
+st.header('Business Case - Le Wagon 869')
+st.subheader('How to analyse and predict sales in a company with different stores, cities and product types')
+st.text('Case study: 54 stores in 22 cities in Ecuador for a commercial company')
 
 #Constante
 Const_Store_nbr = 54
-
+api_token = px.set_mapbox_access_token('pk.eyJ1IjoibXJkYXJhdWpvIiwiYSI6ImNsM3hsY2c2NzAzcHEzYm1oYmliZHc5aXoifQ.1E3p2I8p8bEkHSPJDzUXWQ')
 
 # Load csv dataset
 data_train = pd.read_csv("/Users/farahboukitab/code/mrdaraujo/business_case_869/business_case_869/data/store-sales-time-series-forecasting/train.csv")
 data_stores = pd.read_csv("/Users/farahboukitab/code/mrdaraujo/business_case_869/business_case_869/data/store-sales-time-series-forecasting/stores.csv")
+
+df_heatmap = pd.read_csv("/Users/farahboukitab/code/mrdaraujo/business_case_869/business_case_869/data/store-sales-time-series-forecasting/Heatmap.csv")
+df_heatmap.rename(columns = {'Unnamed: 0':'city', 'Unnamed: 1':'Lat', 'Unnamed: 2':'Lon', 'Unnamed: 3':'Weight'}, inplace = True)
+df_heatmap.drop([0], axis=0, inplace=True)
+
 # merge train dataset and stores dataset
 data_train_merge_stores = pd.merge(data_train, data_stores, on="store_nbr")
 data_train_merge_stores['date'] = pd.to_datetime(data_train_merge_stores['date'])
+#Drop year 2017 because it is not full
+data_train_merge_stores = data_train_merge_stores.drop(data_train_merge_stores[data_train_merge_stores['year']==2017].index, axis=0, inplace=False)
+
+#merge train dataset and stores dataset with heatmap
+map_base = pd.merge(data_train_merge_stores, df_heatmap, on='city').drop(columns=['Weight', 'type', 'cluster', 'state', 'onpromotion'])
+map_base['date'] = pd.to_datetime(map_base['date'])
+map_base['month'] = map_base['date'].dt.month
+map_base['year'] = map_base['date'].dt.year
+#map_base = map_base.drop(map_base[map_base['year']==2017].index, axis=0, inplace=False)
+map_base['Lat'] = pd.to_numeric(map_base['Lat'])
+map_base['Lon'] = pd.to_numeric(map_base['Lon'])
+sales_city_year = pd.DataFrame(map_base.groupby(['city', 'Lat', 'Lon'])['sales'].sum()).reset_index().sort_values(by='sales', ascending=False)
+
 # Create baseline dataset
 data_baseline = data_train_merge_stores
 data_baseline['month'] = data_baseline['date'].dt.month
 data_baseline['year'] = data_baseline['date'].dt.year
-data_baseline = data_baseline[data_baseline['year']<2017]
+#data_baseline = data_baseline[data_baseline['year']<2017]
+
+#Create dataframe for top 5 cities
+map_base_top_five = map_base[map_base['city'].isin(['Quito', 'Guayaquil', 'Cuenca', 'Ambato', 'Santo Domingo'])]
+map_base_top_five = pd.DataFrame(map_base_top_five.groupby(['city', 'year'])['sales'].sum()).reset_index().sort_values(by=[ 'year','sales', 'city' ], ascending=[True,False , True])
+map_base_top_five['year'] = map_base_top_five['year'].astype('string')
+
+#Create dataframe with cities and number of stores in each city
+df_stores_city = pd.DataFrame(data_stores.groupby(['city'])['store_nbr'].count())
+df_stores_city = df_stores_city.reset_index().sort_values(by='store_nbr', ascending=False)
 
 
 # Create a block on the left - Selection
@@ -70,8 +99,34 @@ with st.sidebar:
     st.write('Selected family:')
     st.table(family_selection)
 
-# Create a first part for analysis on city and store
-st.header("1)Analysis on city and store")
+st.header("1) Data visualization")
+
+# Create a first part for analysis on cities
+with st.expander("1.1) General analysis on sales and cities"):
+    #col1, col2 = st.columns(2)
+    #with col1:
+    st.caption("a) Map with cities and sales")
+    px.set_mapbox_access_token('pk.eyJ1IjoibXJkYXJhdWpvIiwiYSI6ImNsM3hsY2c2NzAzcHEzYm1oYmliZHc5aXoifQ.1E3p2I8p8bEkHSPJDzUXWQ')
+    df = px.data.election_geojson()
+    fig = px.scatter_mapbox(data_frame=sales_city_year, lat="Lat", lon="Lon", color="city", size="sales",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=5, mapbox_style="carto-darkmatter")
+    st.write(fig)
+    #with col2:
+    st.caption("b) Sales for top 5 cities")
+    fig = px.bar(map_base_top_five, x='city', y='sales', color='year')
+    st.write(fig)
+    st.caption("c) Prediction based on the city")
+    st.write("Let's predict the number of sales for the city ", city_selection, " for 12 months of the year ", date_selection.year)
+
+
+#Create a second part for analysis on stores
+with st.expander("1.2)General analysis on sales and stores") :
+    st.caption("a) Number on stores on each city")
+    fig = px.bar(data_frame=df_stores_city, x='city', y='store_nbr')
+    st.write(fig)
+
+
+
 data_selection_bycity = data_baseline[data_baseline['city'] == city_selection]
 data_selection_bycity = pd.DataFrame(data_selection_bycity.groupby(['store_nbr'])['sales'].sum()).reset_index()
 
@@ -84,49 +139,13 @@ if len(array_city_selection_store) > 1 :
                 " for the ",len(array_city_selection_store),
                 " stores from 2013 to 2016 ")
 
-        #fig, ax = plt.subplots()
-        #ax.bar(data_selection_bycity['store_nbr'].astype("string"),data_selection_bycity['sales'])
-        #st.pyplot(fig)
-
-        plt.style.use('_mpl-gallery')
-
-        # make the data
-        x = data_selection_bycity['store_nbr']
-        y = data_selection_bycity['sales']
-        # size and color:
-        sizes = np.random.uniform(15, 80, len(x))
-        colors = np.random.uniform(15, 80, len(x))
-
-        # plot
         fig, ax = plt.subplots()
-        ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
-        ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-        ylim=(0, 8), yticks=np.arange(1, 8))
+        ax.bar(data_selection_bycity['store_nbr'].astype("string"),data_selection_bycity['sales'])
         st.pyplot(fig)
-        #plt.show()
-
-
-
-
-
-
-
-
-
 
 elif len(array_city_selection_store) == 1 :
     st.write('There is only one store in the city that has sold ',int(data_selection_bycity['sales'].sum()),
                     ' products from 2013 to 2016')
-
-
-
-
-
-
-
-
-
-
 
 
 #Plot sales of each year for the selected city and store
