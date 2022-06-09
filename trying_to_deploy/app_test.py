c86a45b3f0f98fb49813730bc546c91701570d73
marcio ramos <marciorda96@gmail.com>
2022-06-08 21:30:53 -0300

    Finishing one more day. The Facebook Prophet model is safe.

---
+++ trying_to_deploy/app_test.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
